69c5a3b550e023a2ba78c3cd59b7c8be4d013261
marcaoweb <marcuswebdesigner@yahoo.com.br>
2021-07-24 17:19:56 -0300

    fixed error

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Slider from "react-slick";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import TextField, {Input} from '@material/react-text-field';
